51b5b392f73bde7e34f22827e2cd7e97a8f5a5d0
rias-z <chestermx88@gmail.com>
2018-03-03 02:16:35 +0900

    fix: eslint修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'react/jsx-filename-extension': [
       1, {'extensions': ['.js', '.jsx']}
     ],
+    'react/jsx-indent': 0,
     'react/prop-types': 0,
     'semi': [WARN, 'never'],
   },
--- src/containers/PublicMessage/api/index.js
+++ src/containers/PublicMessage/api/index.js
@@ -8,13 +8,12 @@ export const apiPostComment = async (token, roomId, content) => {
   const res = await request
     .post(endpoint + '/message/post')
     .set({ Authorization: token })
-    .send(
-      {
-        roomId: roomId,
-        messageType: 'public',
-        channelId: 1,
-        content: content,
-      })
+    .send({
+      roomId: roomId,
+      messageType: 'public',
+      channelId: 1,
+      content: content,
+    })
 
   return res.body
 }
