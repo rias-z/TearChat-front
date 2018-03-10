3775b1be2351c05f930df72baad1c0afd7d2dea5
rias-z <chestermx88@gmail.com>
2018-03-10 15:04:03 +0900

    fix: firefoxやsafariを使えるようにした

--- src/index.js
+++ src/index.js
@@ -3,21 +3,15 @@ import ReactDOM from 'react-dom'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducer'
 
 import App from './containers/App'
 
 
-const devtool =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
 const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(thunk),
-    devtool
-  )
+  applyMiddleware(thunk),
 )
 
 ReactDOM.render(
