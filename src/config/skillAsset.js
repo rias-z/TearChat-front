7a04e4a539d46e549ae7407587ef8b5336565ca4
rias-z <chestermx88@gmail.com>
2018-05-11 14:27:04 +0900

    feat|fix: PCデータ追加

--- src/config/skillAsset.js
+++ src/config/skillAsset.js
@@ -4,18 +4,64 @@ export const skillAsset = [
     displayName: '戦闘技能',
     contents: [
       {
-        name: 'avoidance',
+        name: 'dodge',
         displayName: '回避',
+        initialPoint: 0,
       },
       {
         name: 'kick',
         displayName: 'キック',
-        initialPoint: 50,
+        initialPoint: 25,
+      },
+      {
+        name: 'grapped',
+        displayName: '組み付き',
+        initialPoint: 25,
       },
       {
         name: 'punch',
-        displayName: 'パンチ',
+        displayName: 'こぶし(パンチ)',
         initialPoint: 50,
+      },
+      {
+        name: 'headButt',
+        displayName: '頭突き',
+        initialPoint: 10,
+      },
+      {
+        name: 'throwing',
+        displayName: '投擲',
+        initialPoint: 25,
+      },
+      {
+        name: 'martialArts',
+        displayName: 'マーシャルアーツ',
+        initialPoint: 1,
+      },
+      {
+        name: 'gun',
+        displayName: '拳銃',
+        initialPoint: 20,
+      },
+      {
+        name: 'submachineGun',
+        displayName: 'サブマシンガン',
+        initialPoint: 15,
+      },
+      {
+        name: 'shotGun',
+        displayName: 'ショットガン',
+        initialPoint: 30,
+      },
+      {
+        name: 'machineGun',
+        displayName: 'マシンガン',
+        initialPoint: 15,
+      },
+      {
+        name: 'rifle',
+        displayName: 'ライフル',
+        initialPoint: 25,
       },
     ]
   },
@@ -26,10 +72,250 @@ export const skillAsset = [
       {
         name: 'firstAid',
         displayName: '応急手当',
+        initialPoint: 30,
       },
       {
-        name: 'unlocking',
+        name: 'locksmith',
         displayName: '鍵開け',
+        initialPoint: 1,
+      },
+      {
+        name: 'hide',
+        displayName: '隠す',
+        initialPoint: 15,
+      },
+      {
+        name: 'hideout',
+        displayName: '隠れる',
+        initialPoint: 10,
+      },
+      {
+        name: 'listen',
+        displayName: '聞き耳',
+        initialPoint: 25,
+      },
+      {
+        name: 'sneaking',
+        displayName: '忍び歩き',
+        initialPoint: 10,
+      },
+      {
+        name: 'photography',
+        displayName: '写真術',
+        initialPoint: 10,
+      },
+      {
+        name: 'psychoanalysis',
+        displayName: '精神分析',
+        initialPoint: 1,
+      },
+      {
+        name: 'pursuit',
+        displayName: '追跡',
+        initialPoint: 10,
+      },
+      {
+        name: 'climbing',
+        displayName: '登攀',
+        initialPoint: 40,
+      },
+      {
+        name: 'libraryUse',
+        displayName: '図書館',
+        initialPoint: 25,
+      },
+      {
+        name: 'spotHidden',
+        displayName: '目星',
+        initialPoint: 25,
+      },
+    ]
+  },
+  {
+    name: 'action',
+    displayName: '行動技能',
+    contents: [
+      {
+        name: 'driving',
+        displayName: '運転',
+        initialPoint: 20,
+      },
+      {
+        name: 'mechanicalRepair',
+        displayName: '機械修理',
+        initialPoint: 20,
+      },
+      {
+        name: 'heavyMachineOperation',
+        displayName: '重機械操作',
+        initialPoint: 1,
+      },
+      {
+        name: 'horseRiding',
+        displayName: '乗馬',
+        initialPoint: 5,
+      },
+      {
+        name: 'swimming',
+        displayName: '水泳',
+        initialPoint: 25,
+      },
+      {
+        name: 'manufacture',
+        displayName: '製作',
+        initialPoint: 5,
+      },
+      {
+        name: 'maneuver',
+        displayName: '操縦',
+        initialPoint: 1,
+      },
+      {
+        name: 'jumping',
+        displayName: '跳躍',
+        initialPoint: 25,
+      },
+      {
+        name: 'electricalRepair',
+        displayName: '電気修理',
+        initialPoint: 10,
+      },
+      {
+        name: 'navigate',
+        displayName: 'ナビゲート',
+        initialPoint: 10,
+      },
+      {
+        name: 'disguise',
+        displayName: '変装',
+        initialPoint: 1,
+      },
+    ]
+  },
+  {
+    name: 'negotiation',
+    displayName: '交渉技能',
+    contents: [
+      {
+        name: 'glib',
+        displayName: '言いくるめ',
+        initialPoint: 5,
+      },
+      {
+        name: 'creditRating',
+        displayName: '信用',
+        initialPoint: 15,
+      },
+      {
+        name: 'persuasion',
+        displayName: '説得',
+        initialPoint: 15,
+      },
+      {
+        name: 'haggling',
+        displayName: '値切り',
+        initialPoint: 5,
+      },
+    ]
+  },
+  {
+    name: 'knowledge',
+    displayName: '知識技能',
+    contents: [
+      {
+        name: 'medicine',
+        displayName: '医学',
+        initialPoint: 5,
+      },
+      {
+        name: 'occult',
+        displayName: 'オカルト',
+        initialPoint: 5,
+      },
+      {
+        name: 'chemical',
+        displayName: '化学',
+        initialPoint: 1,
+      },
+      {
+        name: 'cthulhuMyth',
+        displayName: 'クトゥルフ神話',
+        initialPoint: 0,
+      },
+      {
+        name: 'art',
+        displayName: '芸術',
+        initialPoint: 5,
+      },
+      {
+        name: 'accounting',
+        displayName: '経理',
+        initialPoint: 10,
+      },
+      {
+        name: 'archeology',
+        displayName: '考古学',
+        initialPoint: 1,
+      },
+      {
+        name: 'computer',
+        displayName: 'コンピューター',
+        initialPoint: 1,
+      },
+      {
+        name: 'psychology',
+        displayName: '心理学',
+        initialPoint: 5,
+      },
+      {
+        name: 'anthropology',
+        displayName: '人類学',
+        initialPoint: 1,
+      },
+      {
+        name: 'biology',
+        displayName: '生物学',
+        initialPoint: 1,
+      },
+      {
+        name: 'geology',
+        displayName: '地質学',
+        initialPoint: 1,
+      },
+      {
+        name: 'electronics',
+        displayName: '電子工学',
+        initialPoint: 1,
+      },
+      {
+        name: 'astronomy',
+        displayName: '天文学',
+        initialPoint: 1,
+      },
+      {
+        name: 'naturalHistory',
+        displayName: '博物学',
+        initialPoint: 10,
+      },
+      {
+        name: 'physics',
+        displayName: '物理学',
+        initialPoint: 1,
+      },
+      {
+        name: 'law',
+        displayName: '法律',
+        initialPoint: 5,
+      },
+      {
+        name: 'pharmary',
+        displayName: '薬学',
+        initialPoint: 1,
+      },
+      {
+        name: 'history',
+        displayName: '歴史',
+        initialPoint: 20,
       },
     ]
   },
